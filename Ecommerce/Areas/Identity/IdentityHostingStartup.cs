725d0046ad3379ef9a928ac70e3d31b1847271ea
visrut navadiya <visrutnavadiya@gmail.com>
2021-09-22 09:28:32 +0530

    Initial commit

---
+++ Ecommerce/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Ecommerce.Areas.Identity.Data;
+using Ecommerce.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Ecommerce.Areas.Identity.IdentityHostingStartup))]
+namespace Ecommerce.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EcommerceDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EcommerceDBContextConnection")));
+
+                services.AddDefaultIdentity<EcommerceAppUser>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<EcommerceDBContext>();
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# E-Commerce-WDDN-Core
+
+- start project : `dotnet new mvc --auth Individual`
+- add gitignore : `dotnet new gitignore`
+- compile on save : `dotnet watch run`
